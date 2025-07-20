1293b3162f045fcaa676f1a5952290c2dcda30f5
Dionysios <dionysios8292@gmail.com>
2025-07-21 00:05:06 +0100

    asdf

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,23 +1,26 @@
-import React from 'react'
+import React from "react";
 
 export default function Footer() {
   return (
-    <footer className="bg-gray-900 text-white py-12" id="section3">
-          <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-            <div className="text-center">
-              <h3 className="text-2xl font-bold mb-4">
-                Ready to Start Your Journey?
-              </h3>
-              <p className="text-gray-400 mb-8">
-                Download now and begin exploring Greek geography today!
-              </p>
-              <a href="#download-buttons">
-                <button  className="bg-blue-600 text-white px-8 py-3 rounded-full font-semibold hover:bg-blue-700 transition">
-                  Get Started
-                </button>
-              </a>
-            </div>
-          </div>
-        </footer>
-  )
+    <div  id="section3">
+
+    <footer className="bg-gray-900 text-white py-12">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="text-center">
+          <h3 className="text-2xl font-bold mb-4">
+            Έτοιμος να ξεκινήσεις το ταξίδι σου;
+          </h3>
+          <p className="text-gray-400 mb-8">
+           Κατέβασε την εφαρμογή τώρα και άρχισε να εξερευνάς τη γεωγραφία της Ελλάδας!
+          </p>
+          <a href="#download-buttons">
+            <button className="bg-blue-600 text-white px-8 py-3 rounded-full font-semibold hover:bg-blue-700 transition">
+              Ξεκίνα
+            </button>
+          </a>
+        </div>
+      </div>
+    </footer>
+    </div>
+  );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -28,6 +28,7 @@ export default function Header() {
         <h1 className="text-3xl sm:text-4xl md:text-4xl lg:text-5xl xl:text-5xl font-bold text-white mb-4 sm:mb-8 lg:mb-20">
           με Διασκεδαστικά Κουίζ
         </h1>
+        <div id="download-buttons">
         <p className="text-md lg:text-2xl text-gray-200 pb-20 pt-10 max-w-2xl mx-auto">
           Μάθε για τα όμορφα νησιά, τις ιστορικές πόλεις, τα μεγαλοπρεπή βουνά
           και τα εντυπωσιακά τοπία. Ιδανικό για μαθητές, ταξιδιώτες και λάτρεις
@@ -41,40 +42,44 @@ export default function Header() {
             Learn More
           </button>
         </div> */}
+          <div className="block sm:hidden text-[20px] text-gray-700 font-medium">
+            Κατέβασε την εφαρμογή τώρα!
+          </div>
+          <div className="flex flex-row justify-center gap-4 mb-8 mt-10">
+            {/* <div className="flex flex-col sm:flex-row justify-center gap-4 mb-8 mt-10"> */}
+            <a
+              // href="#"
+              href="https://apps.apple.com/app/id6504780092"
+              target="_blank"
+              className="inline-flex items-center bg-black text-white px-8 py-3 rounded-full font-semibold hover:bg-gray-800 transition"
+            >
+              <AiFillApple className="text-white mr-2" size={24} />
+              {/* <Apple className="w-6 h-6 mr-2" /> */}
+              <div className="hidden sm:block  mr-2">Κατέβασμα για </div>
+              <div>iOS</div>
+            </a>
 
-        <div id='download-buttons' className="block sm:hidden text-[20px] text-gray-700 font-medium">
-          Κατέβασε την εφαρμογή τώρα!
-        </div>
-        <div className="flex flex-row justify-center gap-4 mb-8 mt-10">
-          {/* <div className="flex flex-col sm:flex-row justify-center gap-4 mb-8 mt-10"> */}
-          <a
-            // href="#"
-            href="https://apps.apple.com/app/id6504780092"
-            target="_blank"
-            className="inline-flex items-center bg-black text-white px-8 py-3 rounded-full font-semibold hover:bg-gray-800 transition"
-          >
-            <AiFillApple className="text-white mr-2" size={24} />
-            {/* <Apple className="w-6 h-6 mr-2" /> */}
-            <div className="hidden sm:block  mr-2">Κατέβασμα για </div>
-            <div>iOS</div>
-          </a>
+            <a
+              // href="#"
+              href="https://play.google.com/store/apps/details?id=com.greekgeographyquizapp.dion"
+              target="_blank"
+              className="inline-flex items-center bg-green-600 text-white px-8 py-3 rounded-full font-semibold hover:bg-green-700 transition"
+            >
+              <AiFillAndroid className="text-white mr-2" size={24} />
+              {/* <Android className="w-6 h-6 mr-2" /> */}
 
-          <a
-            // href="#"
-            href="https://play.google.com/store/apps/details?id=com.greekgeographyquizapp.dion"
-            target="_blank"
-            className="inline-flex items-center bg-green-600 text-white px-8 py-3 rounded-full font-semibold hover:bg-green-700 transition"
-          >
-            <AiFillAndroid className="text-white mr-2" size={24} />
-            {/* <Android className="w-6 h-6 mr-2" /> */}
-
-            <div className="hidden sm:block  mr-2">Κατέβασμα για </div>
-            <div>Android</div>
-          </a>
+              <div className="hidden sm:block  mr-2">Κατέβασμα για </div>
+              <div>Android</div>
+            </a>
+          </div>
         </div>
 
         <div>
-          <img src="/GreekGeographySmartLink.png" alt="QR code" className="rounded-lg shadow-2xl w-1/3 md:w-1/5 lg:w-1/6 mx-auto mt-20" />
+          <img
+            src="/GreekGeographySmartLink.png"
+            alt="QR code"
+            className="rounded-lg shadow-2xl w-1/3 md:w-1/5 lg:w-1/6 mx-auto mt-20"
+          />
         </div>
         <div className="mb-30" />
         {/* <button className="bg-white text-blue-600 px-8 py-3 rounded-full font-semibold border-2 border-blue-600 hover:bg-blue-50 transition">
